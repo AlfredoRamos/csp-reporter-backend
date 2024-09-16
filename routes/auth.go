527ae992b44310230e880a46895761415d8e08b6
Alfredo Ramos <alfredo.ramos@proton.me>
2024-09-15 21:36:19 -0600

    Code cleanup

--- routes/auth.go
+++ routes/auth.go
@@ -7,16 +7,19 @@ import (
 )
 
 func RegisterAuthRoutes(g fiber.Router) {
+	// Auth
+	g.Use(middlewares.AuthLimiter())
+
 	// Public
-	g.Post("/login", middlewares.AuthLimiter(), middlewares.CaptchaProtected(), controllers.AuthLogin)
-	g.Post("/register", middlewares.AuthLimiter(), middlewares.CaptchaProtected(), controllers.AuthRegister)
-	g.Post("/recover", middlewares.AuthLimiter(), middlewares.CaptchaProtected(), controllers.AuthRecover)
-	g.Post("/recover/validate", middlewares.AuthLimiter(), controllers.AuthRecoverValidate) // Without captcha protection
-	g.Patch("/recover/update", middlewares.AuthLimiter(), middlewares.CaptchaProtected(), controllers.AuthRecoverUpdate)
+	g.Post("/login", middlewares.CaptchaProtected(), controllers.AuthLogin)
+	g.Post("/register", middlewares.CaptchaProtected(), controllers.AuthRegister)
+	g.Post("/recover", middlewares.CaptchaProtected(), controllers.AuthRecover)
+	g.Post("/recover/validate", controllers.AuthRecoverValidate) // Without captcha protection
+	g.Patch("/recover/update", middlewares.CaptchaProtected(), controllers.AuthRecoverUpdate)
 
 	// Private
 	g.Use(middlewares.AuthProtected(), middlewares.ValidateJWT(), middlewares.CheckPermissions())
 	g.Post("/check", controllers.AuthCheck)
 	g.Post("/logout", controllers.AuthLogout)
-	g.Patch("/refresh", middlewares.AuthLimiter(), controllers.AuthRefresh)
+	g.Patch("/refresh", controllers.AuthRefresh)
 }
