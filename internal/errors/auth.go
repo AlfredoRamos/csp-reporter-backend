180d7502d86a3632d1985ed123024b2ce74ee579
Alfredo Ramos <alfredo.ramos@proton.me>
2025-01-15 20:45:53 -0600

    Add error code when access or refresh token expired

--- internal/errors/auth.go
+++ internal/errors/auth.go
@@ -3,6 +3,8 @@ package errors
 import "errors"
 
 var (
+	ErrExpiredAccessToken     = errors.New("The access token has expired.")
+	ErrExpiredRefreshToken    = errors.New("The refresh token has expired.")
 	ErrAuthShortPassword      = errors.New("The password length is lower than the minimum allowed.")
 	ErrAuthWeakPassword       = errors.New("The password is not strong enough.")
 	ErrAuthLowEntropyPassword = errors.New("The password entropy is low.")
